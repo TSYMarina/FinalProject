990644b51f89fa1a4bb07e1bbfe64c286d3f9638
Marina Tsymlianskaia <96967608+TSYMarina@users.noreply.github.com>
2022-04-04 20:24:44 -0600

    working demo :)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace FinalProject.Controllers
             return View();
         }
 
+        public IActionResult Words()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
